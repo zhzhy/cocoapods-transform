0abf2531b5056675c670d3d05491108bbde62d1c
从权 <chaoyang.zcy@alibaba-inc.com>
2016-08-01 17:43:08 +0800

    [update]第一版

--- cocoapods-transform.gemspec
+++ cocoapods-transform.gemspec
@@ -6,11 +6,11 @@ require 'cocoapods-transform/gem_version.rb'
 Gem::Specification.new do |spec|
   spec.name          = 'cocoapods-transform'
   spec.version       = CocoapodsTransform::VERSION
-  spec.authors       = ['从权']
-  spec.email         = ['chaoyang.zcy@alibaba-inc.com']
+  spec.authors       = ['张朝阳']
+  spec.email         = ['zhzhy@158.com']
   spec.description   = %q{A short description of cocoapods-transform.}
   spec.summary       = %q{A longer description of cocoapods-transform.}
-  spec.homepage      = 'https://github.com/EXAMPLE/cocoapods-transform'
+  spec.homepage      = 'https://github.com/zhzhy/cocoapods-generator'
   spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
@@ -18,6 +18,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
+  spec.add_dependency "cocoapods", "~> 0.39"
   spec.add_development_dependency 'bundler', '~> 1.3'
   spec.add_development_dependency 'rake'
 end
--- lib/cocoapods-transform/command/transform.rb
+++ lib/cocoapods-transform/command/transform.rb
@@ -18,26 +18,64 @@ module Pod
     #       in the `plugins.json` file, once your plugin is released.
     #
     class Transform < Command
-      self.summary = 'Short description of cocoapods-transform.'
-
+      self.summary = 'Transform Podfile between source Code and Framework.'
+      self.command = 'transform'
       self.description = <<-DESC
-        Longer description of cocoapods-transform.
+        With this command, users can transform Podfile between source Code and Framework,
+        very useful for Debug, code searching, code replacing and so on.
       DESC
-
-      self.arguments = 'NAME'
-
-      def initialize(argv)
-        @name = argv.shift_argument
-        super
-      end
 
       def validate!
         super
-        help! 'A Pod name is required.' unless @name
       end
 
       def run
-        UI.puts "Add your implementation for the cocoapods-transform plugin in #{__FILE__}"
+        path = podfile_path
+        if !path
+          help! 'Make sure a Podfile at current directory!'
+        end
+        pods = parse_Podfile(path)
+        print("#{pods}")
+        transform(path, pods)
+      end
+
+      def podfile_path
+        podfile_path = Dir.pwd+'/Podfile'
+        if File.exist?(podfile_path)
+          return podfile_path
+        else
+          return
+        end
+      end
+
+      def parse_Podfile(podfile_path)
+        active_pods = Array.new
+        inactive_pods = Array.new
+        File.open(podfile_path, 'r') do |f|
+          active_pod_reg = /^[\t ]*Pod *('.*')[\n\r,]$/
+          inactive_pod_reg = /^[#\t ]*Pod *('.*')[\n\r,]$/
+          f.each_line do |line|
+            if active_pod_reg.match(line)
+              active_pods.push($1)
+            elsif inactive_pod_reg.match(line)
+              inactive_pods.push($1)
+            end
+          end
+        end
+        print("#{active_pods}")
+        return active_pods & inactive_pods
+      end
+
+      def transform(podfile_path, pods)
+        content = File.read(podfile_path)
+        for pod in pods
+          content = content.gsub(/^[\t\f]*Pod/, '#Pod')
+          content = content.gsub(/^[#\t\f]*Pod/, 'Pod')
+        end
+
+        File.open(podfile_path, 'w') do |f|
+          f.puts(content)
+        end
       end
     end
   end
